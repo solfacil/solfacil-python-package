59b8bf4173f8be1f9e2c165a1940f11ee27fe5f6
LuizFerraresi <luiz.ferraresi@solfacil.com.br>
2025-08-04 09:12:12 -0300

    add initial config for single node and cluster

--- solfacil/cache/adapter.py
+++ solfacil/cache/adapter.py
@@ -1,12 +1,12 @@
-import logging
 from contextlib import asynccontextmanager
 from typing import Any
+import logging
 
+from redis.asyncio.connection import ConnectionPool
 from redis.asyncio.cluster import RedisCluster
 from redis.asyncio.client import Redis
 from redis.asyncio.retry import Retry
 from redis.backoff import ExponentialBackoff
-from redis.connection import ConnectionPool
 
 from .settings import CacheRedisClusterSettings, CacheRedisModeSettings, CacheRedisSingleNodeSettings
 from .constants import CacheRedisMode
@@ -16,30 +16,36 @@ logger = logging.getLogger(__name__)
 
 class CacheRedisAdapter:
     
-    @staticmethod
-    def __get_config(mode: CacheRedisModeSettings) -> CacheRedisClusterSettings | CacheRedisSingleNodeSettings:
-        return (
-            CacheRedisClusterSettings() 
-            if mode.deployment_mode == CacheRedisMode.CLUSTER 
-            else CacheRedisSingleNodeSettings()
-        )
+    @classmethod
+    def single_node_config(cls) -> "CacheRedisAdapter":
+        settings = CacheRedisSingleNodeSettings()
+        return cls(settings)
+    
+    @classmethod
+    def cluster_config(cls) -> "CacheRedisAdapter":
+        settings = CacheRedisClusterSettings()
+        return cls(settings)
     
     @classmethod
     def config(cls) -> "CacheRedisAdapter":
         cache_mode_settings = CacheRedisModeSettings()
-        cache_adapter_settings = cls.__get_config(cache_mode_settings)
-        logger.info(f"[ADAPTER][CACHE][CONNECTION URI: {cache_adapter_settings.build_uri}]")
-        logger.info(f"[ADAPTER][CACHE][CONNECTION MODE: {cache_adapter_settings.deployment_mode.value.upper()}]")
-        return cls(cache_adapter_settings)
+        return (
+            cls.cluster_config()
+            if cache_mode_settings.deployment_mode == CacheRedisMode.CLUSTER 
+            else cls.single_node_config()
+        )
         
     def __init__(self, settings: CacheRedisClusterSettings | CacheRedisSingleNodeSettings) -> None:
-        self._connection_pool: ConnectionPool | None = None
+        # self._connection_pool: ConnectionPool | None = None
         self._single_node_connection: Redis | None = None
         self._cluster_connection: RedisCluster | None = None
         self._settings = settings
         
+        logger.info(f"[ADAPTER][CACHE][CONNECTION URI: {self._settings.build_uri}]")
+        logger.info(f"[ADAPTER][CACHE][CONNECTION MODE: {self._settings.deployment_mode.value.upper()}]")
+        
     @property
-    def _retry_config(self) -> dict[str, Any]:
+    def __retry_config(self) -> dict[str, Any]:
         retry_config = {
             'retry': Retry(ExponentialBackoff(), retries=2),
             'retry_on_error': [ConnectionError, TimeoutError],
@@ -47,10 +53,11 @@ class CacheRedisAdapter:
         return retry_config
     
     @property
-    def _common_config(self) -> dict[str, Any]:
+    def __common_config(self) -> dict[str, Any]:
         common_config = {
             'host': self._settings.host,
             'port': self._settings.port,
+            'db': self._settings.db,
             'socket_timeout': self._settings.socket_timeout,
             'socket_keepalive': self._settings.socket_keepalive,
             'socket_connect_timeout': self._settings.socket_connect_timeout,
@@ -60,37 +67,40 @@ class CacheRedisAdapter:
         return common_config
     
     @property
-    def cluster_config(self) -> dict[str, Any]:
+    def __cluster_config(self) -> dict[str, Any]:
         cluster_config = {
-            **self._common_config,
+            **self.__common_config,
+            **self.__retry_config,
+            'read_from_replicas': self._settings.read_from_replicas,
             'require_full_coverage': self._settings.require_full_coverage,
         }
         return cluster_config
-
+    
     @property
-    def single_node_config(self) -> dict[str, Any]:
+    def __single_node_config(self) -> dict[str, Any]:
         single_node_config = {
-            **self._common_config,
+            **self.__common_config,
             'retry_on_timeout': self._settings.retry_on_timeout,
         }
         return single_node_config
     
     def __create_cluster_connection(self) -> RedisCluster:
-        self._cluster_connection = RedisCluster(**self.cluster_config)
+        self._cluster_connection = RedisCluster(**self.__cluster_config)
         
     def __create_single_node_connection(self) -> Redis:
         # self._connection_pool = ConnectionPool(**self.single_node_config)
         # self._single_node_connection = Redis(connection_pool=self._connection_pool)
-        self._single_node_connection = Redis(**self.single_node_config)
+        self._single_node_connection = Redis(**self.__single_node_config)
         # logger.info(f"[ADAPTER][CACHE][CONNECTION POOL ACTIVE: {self._connection_pool.can_get_connection()}]")
          
     async def connect(self) -> None:
         if self._settings.deployment_mode == CacheRedisMode.CLUSTER:
             self.__create_cluster_connection()
-            logger.info(f"[ADAPTER][CACHE][CLUSTER CONNECTION ACTIVE: {await self._cluster_connection.ping()}]")
+            logger.info(f"[ADAPTER][CACHE][CONNECTION ACTIVE: {await self._cluster_connection.ping()}]")
             logger.info(f"[ADAPTER][CACHE][CLUSTER NODES: {self._cluster_connection.get_nodes()}]")
         else:
             self.__create_single_node_connection()
+            logger.info(f"[ADAPTER][CACHE][CONNECTION ACTIVE: {await self._single_node_connection.ping()}]")
         
     async def __disconnect_cluster_connection(self) -> None:
         if self._cluster_connection:
@@ -98,9 +108,8 @@ class CacheRedisAdapter:
             self._cluster_connection = None
             
     async def __disconnect_single_node_connection(self) -> None:
-        if self._connection_pool:
-            await self._connection_pool.aclose()
-            self._connection_pool = None
+        if self._single_node_connection:
+            await self._single_node_connection.aclose()
             self._single_node_connection = None
 
     async def disconnect(self) -> None:
@@ -116,7 +125,6 @@ class CacheRedisAdapter:
             self._cluster_connection
             if self._settings.deployment_mode == CacheRedisMode.CLUSTER
             else self._single_node_connection
-            # return Redis.from_pool(self._connection_pool)
         )
         async with redis_connection as session:
             yield session
