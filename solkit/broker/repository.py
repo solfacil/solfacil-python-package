601f19a9a2795d47ddefe3cdd2b2d4b8731962ad
LuizFerraresi <luiz.ferraresi@solfacil.com.br>
2025-08-06 18:40:38 -0300

    add parse message method on repository

--- solkit/broker/repository.py
+++ solkit/broker/repository.py
@@ -1,3 +1,6 @@
+import json
+from typing import Any
+
 from .adapter import BrokerKafkaAdapter
 
 
@@ -5,6 +8,15 @@ class BrokerRepository:
     def __init__(self, adapter: BrokerKafkaAdapter) -> None:
         self._adapter = adapter
     
-    async def produce(self, topic: str, message: str) -> None:
-        return await self._adapter._producer.send_and_wait(topic, message)
+    @staticmethod
+    def __parse_message(message: dict[str, Any]) -> bytes:
+        return json.dumps(message).encode("utf-8")
     
+    async def produce(self, topic: str, message: dict[str, Any]) -> None:
+        return await self._adapter._producer.send_and_wait(topic, self.__parse_message(message))
+    
+    # async def healthcheck(self) -> None:
+    #     producer = await self._adapter._producer.send_and_wait("healthcheck", "healthcheck")
+    #     consumer = self._adapter._consumer.subscription()  # list topics subscribed
+    #     consumer = self._adapter._consumer.assignment()  # list partitions assigned
+    #     return producer and consumer
